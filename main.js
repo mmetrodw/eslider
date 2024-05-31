1aa815c15b69eb7c636ffff4ff272b7146cf7f64
mmetrodw <artgure@gmail.com>
2024-06-01 00:44:33 +0300

    update main.js

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ class ESlider {
 		this.isSlidingAllowed = true;
 		this.firstSlideClone = null;
 		this.lastSlideClone = null;
-		this.posInitial = 0;
-		this.posX = 0;
+		this.initialPositionX = 0;
+		this.currentPositionX = 0;
 		this.threshold = 100;
 		this.init();
 	}
@@ -26,6 +26,7 @@ class ESlider {
 		this.elements.nextSlideButton = this.elements.wrapper.querySelector('.es-next-slide');
 		this.elements.prevSlideButton = this.elements.wrapper.querySelector('.es-prev-slide');
 		this.elements.navigationItems = this.elements.wrapper.querySelectorAll('.es-navigation-item');
+		
 		this.totalSlides = this.elements.slides.length;
 		this.slideWidth = this.elements.sliderWrapper.offsetWidth;
 		this.firstSlideClone = this.elements.slides[0].cloneNode(true);
@@ -33,29 +34,43 @@ class ESlider {
  
 		this.elements.sliderWrapper.appendChild(this.firstSlideClone);
 		this.elements.sliderWrapper.insertBefore(this.lastSlideClone, this.elements.slides[0]);
- 
-		this.elements.nextSlideButton.addEventListener('click', () => this.moveToNextSlide());
-		this.elements.prevSlideButton.addEventListener('click', () => this.moveToPrevSlide());
+		// Оновлюємо колекцію слайдів
+    this.elements.slides = this.elements.sliderWrapper.querySelectorAll('.es-slide');
+
+    this.elements.prevSlideButton.addEventListener('click', this.moveToPrevSlide.bind(this));
+		this.elements.nextSlideButton.addEventListener('click', this.moveToNextSlide.bind(this));
 		
 		this.elements.navigationItems.forEach((item, index) => {
 			item.addEventListener('click', () => this.moveToSlideByNavigation(index + 1));
 		});
  
+    // Mouse events
+    this.elements.wrapper.onmousedown = this.dragStart.bind(this);
+
+    // Touch events
+    this.elements.wrapper.addEventListener('touchstart', this.dragStart.bind(this));
+    this.elements.wrapper.addEventListener('touchmove', this.dragAction.bind(this));
+    this.elements.wrapper.addEventListener('touchend', this.dragEnd.bind(this));
+
 		this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
 		this.highlightCurrentNavigationItem();
 	}
  
 	moveToNextSlide() {
+	  console.log('next');
 		if (!this.isSlidingAllowed) return;
 		this.isSlidingAllowed = false;
 		this.currentSlideIndex++;
+		console.log('here')
 		this.updateSlidePosition();
 	}
  
 	moveToPrevSlide() {
+	  console.log('prev')
 		if (!this.isSlidingAllowed) return;
 		this.isSlidingAllowed = false;
 		this.currentSlideIndex--;
+		console.log('here prev')
 		this.updateSlidePosition();
 	}
  
@@ -66,6 +81,50 @@ class ESlider {
 		this.updateSlidePosition();
 	}
 	
+	dragStart(event) {
+    if (!this.isSlidingAllowed) return;
+    event.preventDefault();
+
+    this.initialPositionX = event.type === 'touchstart' ? event.touches[0].clientX : event.clientX;
+
+    document.addEventListener('mouseup', this.dragEnd.bind(this));
+    document.addEventListener('mousemove', this.dragAction.bind(this));
+	}
+	
+	dragAction(event) {
+	  if (!this.isSlidingAllowed) return;
+    event.preventDefault();
+
+    this.currentPositionX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
+    
+    const dragDifference = this.currentPositionX - this.initialPositionX;
+    const currentSliderPosition = this.slideWidth * this.currentSlideIndex;
+
+    this.elements.sliderWrapper.style.left = -currentSliderPosition + dragDifference + 'px';
+	}
+
+  dragEnd(event) {
+    event.preventDefault();
+    if (this.currentPositionX - this.initialPositionX < -this.threshold) {
+      this.moveToNextSlide();
+    } else if (this.currentPositionX - this.initialPositionX > this.threshold) {
+      this.moveToPrevSlide();
+    } else {
+      this.animation(
+        this.elements.sliderWrapper,
+        'left',
+        this.elements.sliderWrapper.offsetLeft,
+        -this.slideWidth * this.currentSlideIndex,
+        'px',
+        500,
+        'easing',
+        null
+      );
+    }
+    
+    document.removeEventListener('mouseup', this.dragEnd.bind(this));
+    document.removeEventListener('mousemove', this.dragAction.bind(this));
+  }
 
 	updateSlidePosition() {
 		this.highlightCurrentNavigationItem();
