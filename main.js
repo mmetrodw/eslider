49c3e6c091a7dc0529551ae9c597de8b05939dd2
mmetrodw <artgure@gmail.com>
2024-06-01 12:34:34 +0300

    modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -1,8 +1,199 @@
+const easingFunctions = {
+	linear: (time) => {
+		return time;
+	},
+	easeInSine: (time) => {
+		return -1 * Math.cos(time * (Math.PI / 2)) + 1;
+	},
+	easeOutSine: (time) => {
+		return Math.sin(time * (Math.PI / 2));
+	},
+	easeInOutSine: (time) => {
+		return -0.5 * (Math.cos(Math.PI * time) - 1);
+	},
+	easeInQuad: (time) => {
+		return time * time;
+	},
+	easeOutQuad: (time) => {
+		return time * (2 - time);
+	},
+	easeInOutQuad: (time) => {
+		return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
+	},
+	easeInCubic: (time) => {
+		return time * time * time;
+	},
+	easeOutCubic: (time) => {
+		const time1 = time - 1;
+		return time1 * time1 * time1 + 1;
+	},
+	easeInOutCubic: (time) => {
+		return time < 0.5 ?
+			4 * time * time * time :
+			(time - 1) * (2 * time - 2) * (2 * time - 2) + 1;
+	},
+	easeInQuart: (time) => {
+		return time * time * time * time;
+	},
+	easeOutQuart: (time) => {
+		const time1 = time - 1;
+		return 1 - time1 * time1 * time1 * time1;
+	},
+	easeInOutQuart: (time) => {
+		const time1 = time - 1;
+		return time < 0.5 ? 8 * time * time * time * time : 1 - 8 * time1 * time1 * time1 * time1;
+	},
+	easeInQuint: (time) => {
+		return time * time * time * time * time;
+	},
+	easeOutQuint: (time) => {
+		const time1 = time - 1;
+		return 1 + time1 * time1 * time1 * time1 * time1;
+	},
+	easeInOutQuint: (time) => {
+		const time1 = time - 1;
+		return time < 0.5 ? 16 * time * time * time * time * time : 1 + 16 * time1 * time1 * time1 * time1 * time1;
+	},
+	easeInExpo: (time) => {
+		if (time === 0) {
+			return 0;
+		}
+		return Math.pow(2, 10 * (time - 1));
+	},
+	easeOutExpo: (time) => {
+		if (time === 1) {
+			return 1;
+		}
+		return (-Math.pow(2, -10 * time) + 1);
+	},
+	easeInOutExpo: (time) => {
+		if (time === 0 || time === 1) {
+			return time;
+		}
+
+		const scaledTime = time * 2;
+		const scaledTime1 = scaledTime - 1;
+
+		if (scaledTime < 1) {
+			return 0.5 * Math.pow(2, 10 * (scaledTime1));
+		}
+
+		return 0.5 * (-Math.pow(2, -10 * scaledTime1) + 2);
+	},
+	easeInCirc: (time) => {
+		const scaledTime = time / 1;
+		return -1 * (Math.sqrt(1 - scaledTime * time) - 1);
+	},
+	easeOutCirc: (time) => {
+		const time1 = time - 1;
+		return Math.sqrt(1 - time1 * time1);
+	},
+	easeInOutCirc: (time) => {
+		const scaledTime = time * 2;
+		const scaledTime1 = scaledTime - 2;
+
+		if (scaledTime < 1) {
+			return -0.5 * (Math.sqrt(1 - scaledTime * scaledTime) - 1);
+		}
+
+		return 0.5 * (Math.sqrt(1 - scaledTime1 * scaledTime1) + 1);
+	},
+	easeInBack: (time, magnitude = 1.70158) => {
+		return time * time * ((magnitude + 1) * time - magnitude);
+	},
+	easeOutBack: (time, magnitude = 1.70158) => {
+		const scaledTime = time / 1 - 1;
+		return (scaledTime * scaledTime * ((magnitude + 1) * scaledTime + magnitude)) + 1;
+	},
+	easeInOutBack: (time, magnitude = 1.70158) => {
+		const scaledTime = time * 2;
+		const scaledTime2 = scaledTime - 2;
+		const s = magnitude * 1.525;
+
+		if (scaledTime < 1) {
+			return 0.5 * scaledTime * scaledTime * ((s + 1) * scaledTime - s);
+		}
+
+		return (0.5 * (scaledTime2 * scaledTime2 * ((s + 1) * scaledTime2 + s) + 2));
+	},
+	easeInElastic: (time, magnitude = 0.7) => {
+		if (time === 0 || time === 1) {
+			return time;
+		}
+
+		const scaledTime = time / 1;
+		const scaledTime1 = scaledTime - 1;
+		const p = 1 - magnitude;
+		const s = p / (2 * Math.PI) * Math.asin(1);
+
+		return -(Math.pow(2, 10 * scaledTime1) * Math.sin((scaledTime1 - s) * (2 * Math.PI) / p));
+	},
+	easeOutElastic: (time, magnitude = 0.7) => {
+		if (time === 0 || time === 1) {
+			return time;
+		}
+
+		const p = 1 - magnitude;
+		const scaledTime = time * 2;
+		const s = p / (2 * Math.PI) * Math.asin(1);
+
+		return (Math.pow(2, -10 * scaledTime) * Math.sin((scaledTime - s) * (2 * Math.PI) / p)) + 1;
+	},
+	easeInOutElastic: (time, magnitude = 0.65) => {
+		if (time === 0 || time === 1) {
+			return time;
+		}
+
+		const p = 1 - magnitude;
+		const scaledTime = time * 2;
+		const scaledTime1 = scaledTime - 1;
+		const s = p / (2 * Math.PI) * Math.asin(1);
+
+		if (scaledTime < 1) {
+			return -0.5 * (Math.pow(2, 10 * scaledTime1) * Math.sin((scaledTime1 - s) * (2 * Math.PI) / p));
+		}
+
+		return (Math.pow(2, -10 * scaledTime1) * Math.sin((scaledTime1 - s) * (2 * Math.PI) / p) * 0.5) + 1;
+	},
+	easeOutBounce: (time) => {
+		const scaledTime = time / 1;
+
+		if (scaledTime < (1 / 2.75)) {
+			return 7.5625 * scaledTime * scaledTime;
+		} else if (scaledTime < (2 / 2.75)) {
+			const scaledTime2 = scaledTime - (1.5 / 2.75);
+			return (7.5625 * scaledTime2 * scaledTime2) + 0.75;
+		} else if (scaledTime < (2.5 / 2.75)) {
+			const scaledTime2 = scaledTime - (2.25 / 2.75);
+			return (7.5625 * scaledTime2 * scaledTime2) + 0.9375;
+		} else {
+			const scaledTime2 = scaledTime - (2.625 / 2.75);
+			return (7.5625 * scaledTime2 * scaledTime2) + 0.984375;
+		}
+	},
+	easeInBounce: (time) => {
+		return 1 - easingFunctions.easeOutBounce(1 - time);
+	},
+	easeInOutBounce: (time) => {
+		if (time < 0.5) {
+			return easingFunctions.easeInBounce(time * 2) * 0.5;
+		}
+		return (easingFunctions.easeOutBounce((time * 2) - 1) * 0.5) + 0.5;
+	}
+};
+
 class ESlider {
-	constructor(containerId) {
+	constructor(options) {
+		this.initialSettings = {
+			slideDuration: 1000,
+			autoSlide: false,
+			autoSlideInterval: 3000
+		};
+		this.settings = this.deepMerge(this.initialSettings, options);
+
 		this.currentSlideIndex = 1;
 		this.elements = {
-			wrapper: document.getElementById(containerId),
+			wrapper: document.getElementById(this.settings.target),
 			sliderWrapper: null,
 			slides: null,
 			nextSlideButton: null,
@@ -30,6 +221,7 @@ class ESlider {
 		
 		this.totalSlides = this.elements.slides.length;
 		this.slideWidth = this.elements.sliderWrapper.offsetWidth;
+		this.threshold = this.slideWidth / 4;
 		this.firstSlideClone = this.elements.slides[0].cloneNode(true);
 		this.lastSlideClone = this.elements.slides[this.totalSlides - 1].cloneNode(true);
  
@@ -50,7 +242,7 @@ class ESlider {
     this.elements.sliderWrapper.addEventListener('mousedown', this.dragStart.bind(this));
     this.elements.sliderWrapper.addEventListener('mousemove', this.dragAction.bind(this));
     this.elements.sliderWrapper.addEventListener('mouseup', this.dragEnd.bind(this));
-    this.elements.sliderWrapper.addEventListener('mouseleave', this.dragEnd.bind(this));
+    this.elements.wrapper.addEventListener('mouseleave', this.dragEnd.bind(this));
 
     // Touch events
     //this.elements.wrapper.addEventListener('touchstart', this.dragStart.bind(this));
@@ -84,20 +276,21 @@ class ESlider {
 	}
 	
 	dragStart(event) {
-    if (!this.isSlidingAllowed) return;
     event.preventDefault();
+    if (!this.isSlidingAllowed) return;
 		this.isDragging = true;
 
     this.initialPositionX = event.type === 'touchstart' ? event.touches[0].clientX : event.clientX;
+    this.currentPositionX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
 	}
 	
 	dragAction(event) {
+    event.preventDefault();
 	  if (!this.isSlidingAllowed) return;
 	  if (!this.isDragging) return;
-    event.preventDefault();
 
     this.currentPositionX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
-    
+
     const dragDifference = this.currentPositionX - this.initialPositionX;
     const currentSliderPosition = this.slideWidth * this.currentSlideIndex;
 
@@ -108,7 +301,8 @@ class ESlider {
     event.preventDefault();
 	  if (!this.isDragging) return;
 		this.isDragging = false;
-    if (this.currentPositionX - this.initialPositionX < -this.threshold) {
+
+		if (this.currentPositionX - this.initialPositionX < -this.threshold) {
       this.moveToNextSlide();
     } else if (this.currentPositionX - this.initialPositionX > this.threshold) {
       this.moveToPrevSlide();
@@ -119,17 +313,20 @@ class ESlider {
         this.elements.sliderWrapper.offsetLeft,
         -this.slideWidth * this.currentSlideIndex,
         'px',
-        500,
-        'easing',
-        null
+        this.settings.slideDuration / 100 * Math.abs(this.currentPositionX - this.initialPositionX) / this.threshold * 100,
+        'easeOutExpo'
       );
     }
   }
 
 	updateSlidePosition() {
 		this.highlightCurrentNavigationItem();
-		this.animation(this.elements.sliderWrapper, 'left', this.elements.sliderWrapper.offsetLeft, -this.slideWidth * this.currentSlideIndex, 'px', 500, 'easing', () => {
+		this.elements.wrapper.classList.add('sliding');
+
+		this.animation(this.elements.sliderWrapper, 'left', this.elements.sliderWrapper.offsetLeft, -this.slideWidth * this.currentSlideIndex, 'px', this.settings.slideDuration, 'easeOutExpo', () => {
 			this.isSlidingAllowed = true;
+			this.elements.wrapper.classList.remove('sliding');
+
 			if(this.currentSlideIndex > this.totalSlides) {
 				this.currentSlideIndex = 1;
 				this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
@@ -154,7 +351,7 @@ class ESlider {
 			}
 			const elapsedTime = currentTime - startTime;
 			const progress = Math.min(elapsedTime / duration, 1);
-			const currentValue = startValue + (endValue - startValue) * progress;
+			const currentValue = startValue + (endValue - startValue) * easingFunctions[easingName](progress);
 			element.style[property] = currentValue + unit;
 
 			if(progress < 1) {
@@ -168,6 +365,29 @@ class ESlider {
 
 		requestAnimationFrame(update);
 	}
+
+	// Method to deeply merge default options with user-provided options
+  deepMerge(target, source) {
+    if (typeof target !== 'object' || target === null) {
+      target = {};
+    }
+    
+    if (typeof source !== 'object' || source === null) {
+      return target;
+    }
+
+    for (const key in source) {
+      if (source[key] instanceof Object && target[key]) {
+        target[key] = this.deepMerge(target[key], source[key]);
+      } else {
+        target[key] = source[key];
+      }
+    }
+
+    return target;
+  }
  }
  
- const slider = new ESlider("test");
+ const slider = new ESlider({
+	target: 'test'
+ });
