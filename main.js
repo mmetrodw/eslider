076af69f3d9384488abf33608c583ac47a12f696
mmetrodw <artgure@gmail.com>
2024-06-03 21:33:03 +0300

    new file:   img/common-columbine-8755584_1920.jpg 	new file:   img/gorilla-8760357_1920.jpg 	new file:   img/red-hair-girl-8158373_1920.jpg 	new file:   img/sunset-166637_1920.jpg 	new file:   img/swan-8242931_1920.jpg 	new file:   img/work-8772016_1920.jpg 	modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -1,192 +1,10 @@
-const easingFunctions = {
-	linear: (time) => {
-		return time;
-	},
-	easeInSine: (time) => {
-		return -1 * Math.cos(time * (Math.PI / 2)) + 1;
-	},
-	easeOutSine: (time) => {
-		return Math.sin(time * (Math.PI / 2));
-	},
-	easeInOutSine: (time) => {
-		return -0.5 * (Math.cos(Math.PI * time) - 1);
-	},
-	easeInQuad: (time) => {
-		return time * time;
-	},
-	easeOutQuad: (time) => {
-		return time * (2 - time);
-	},
-	easeInOutQuad: (time) => {
-		return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
-	},
-	easeInCubic: (time) => {
-		return time * time * time;
-	},
-	easeOutCubic: (time) => {
-		const time1 = time - 1;
-		return time1 * time1 * time1 + 1;
-	},
-	easeInOutCubic: (time) => {
-		return time < 0.5 ?
-			4 * time * time * time :
-			(time - 1) * (2 * time - 2) * (2 * time - 2) + 1;
-	},
-	easeInQuart: (time) => {
-		return time * time * time * time;
-	},
-	easeOutQuart: (time) => {
-		const time1 = time - 1;
-		return 1 - time1 * time1 * time1 * time1;
-	},
-	easeInOutQuart: (time) => {
-		const time1 = time - 1;
-		return time < 0.5 ? 8 * time * time * time * time : 1 - 8 * time1 * time1 * time1 * time1;
-	},
-	easeInQuint: (time) => {
-		return time * time * time * time * time;
-	},
-	easeOutQuint: (time) => {
-		const time1 = time - 1;
-		return 1 + time1 * time1 * time1 * time1 * time1;
-	},
-	easeInOutQuint: (time) => {
-		const time1 = time - 1;
-		return time < 0.5 ? 16 * time * time * time * time * time : 1 + 16 * time1 * time1 * time1 * time1 * time1;
-	},
-	easeInExpo: (time) => {
-		if (time === 0) {
-			return 0;
-		}
-		return Math.pow(2, 10 * (time - 1));
-	},
-	easeOutExpo: (time) => {
-		if (time === 1) {
-			return 1;
-		}
-		return (-Math.pow(2, -10 * time) + 1);
-	},
-	easeInOutExpo: (time) => {
-		if (time === 0 || time === 1) {
-			return time;
-		}
-
-		const scaledTime = time * 2;
-		const scaledTime1 = scaledTime - 1;
-
-		if (scaledTime < 1) {
-			return 0.5 * Math.pow(2, 10 * (scaledTime1));
-		}
-
-		return 0.5 * (-Math.pow(2, -10 * scaledTime1) + 2);
-	},
-	easeInCirc: (time) => {
-		const scaledTime = time / 1;
-		return -1 * (Math.sqrt(1 - scaledTime * time) - 1);
-	},
-	easeOutCirc: (time) => {
-		const time1 = time - 1;
-		return Math.sqrt(1 - time1 * time1);
-	},
-	easeInOutCirc: (time) => {
-		const scaledTime = time * 2;
-		const scaledTime1 = scaledTime - 2;
-
-		if (scaledTime < 1) {
-			return -0.5 * (Math.sqrt(1 - scaledTime * scaledTime) - 1);
-		}
-
-		return 0.5 * (Math.sqrt(1 - scaledTime1 * scaledTime1) + 1);
-	},
-	easeInBack: (time, magnitude = 1.70158) => {
-		return time * time * ((magnitude + 1) * time - magnitude);
-	},
-	easeOutBack: (time, magnitude = 1.70158) => {
-		const scaledTime = time / 1 - 1;
-		return (scaledTime * scaledTime * ((magnitude + 1) * scaledTime + magnitude)) + 1;
-	},
-	easeInOutBack: (time, magnitude = 1.70158) => {
-		const scaledTime = time * 2;
-		const scaledTime2 = scaledTime - 2;
-		const s = magnitude * 1.525;
-
-		if (scaledTime < 1) {
-			return 0.5 * scaledTime * scaledTime * ((s + 1) * scaledTime - s);
-		}
-
-		return (0.5 * (scaledTime2 * scaledTime2 * ((s + 1) * scaledTime2 + s) + 2));
-	},
-	easeInElastic: (time, magnitude = 0.7) => {
-		if (time === 0 || time === 1) {
-			return time;
-		}
-
-		const scaledTime = time / 1;
-		const scaledTime1 = scaledTime - 1;
-		const p = 1 - magnitude;
-		const s = p / (2 * Math.PI) * Math.asin(1);
-
-		return -(Math.pow(2, 10 * scaledTime1) * Math.sin((scaledTime1 - s) * (2 * Math.PI) / p));
-	},
-	easeOutElastic: (time, magnitude = 0.7) => {
-		if (time === 0 || time === 1) {
-			return time;
-		}
-
-		const p = 1 - magnitude;
-		const scaledTime = time * 2;
-		const s = p / (2 * Math.PI) * Math.asin(1);
-
-		return (Math.pow(2, -10 * scaledTime) * Math.sin((scaledTime - s) * (2 * Math.PI) / p)) + 1;
-	},
-	easeInOutElastic: (time, magnitude = 0.65) => {
-		if (time === 0 || time === 1) {
-			return time;
-		}
-
-		const p = 1 - magnitude;
-		const scaledTime = time * 2;
-		const scaledTime1 = scaledTime - 1;
-		const s = p / (2 * Math.PI) * Math.asin(1);
-
-		if (scaledTime < 1) {
-			return -0.5 * (Math.pow(2, 10 * scaledTime1) * Math.sin((scaledTime1 - s) * (2 * Math.PI) / p));
-		}
-
-		return (Math.pow(2, -10 * scaledTime1) * Math.sin((scaledTime1 - s) * (2 * Math.PI) / p) * 0.5) + 1;
-	},
-	easeOutBounce: (time) => {
-		const scaledTime = time / 1;
-
-		if (scaledTime < (1 / 2.75)) {
-			return 7.5625 * scaledTime * scaledTime;
-		} else if (scaledTime < (2 / 2.75)) {
-			const scaledTime2 = scaledTime - (1.5 / 2.75);
-			return (7.5625 * scaledTime2 * scaledTime2) + 0.75;
-		} else if (scaledTime < (2.5 / 2.75)) {
-			const scaledTime2 = scaledTime - (2.25 / 2.75);
-			return (7.5625 * scaledTime2 * scaledTime2) + 0.9375;
-		} else {
-			const scaledTime2 = scaledTime - (2.625 / 2.75);
-			return (7.5625 * scaledTime2 * scaledTime2) + 0.984375;
-		}
-	},
-	easeInBounce: (time) => {
-		return 1 - easingFunctions.easeOutBounce(1 - time);
-	},
-	easeInOutBounce: (time) => {
-		if (time < 0.5) {
-			return easingFunctions.easeInBounce(time * 2) * 0.5;
-		}
-		return (easingFunctions.easeOutBounce((time * 2) - 1) * 0.5) + 0.5;
-	}
-};
-
 class ESlider {
 	constructor(options) {
 		this.initialSettings = {
-			slideDuration: 1000,
-			autoSlide: false,
+			slideDuration: 600,
+			slideDirection: 'horizontal', // horizontal, vertical
+			slideEasing: 'easeOutCirc',
+			autoSlide: true,
 			autoSlideInterval: 3000
 		};
 		this.settings = this.deepMerge(this.initialSettings, options);
@@ -206,188 +24,212 @@ class ESlider {
 		this.isDragging = false;
 		this.firstSlideClone = null;
 		this.lastSlideClone = null;
-		this.initialPositionX = 0;
-		this.currentPositionX = 0;
+		this.initialPosition = 0;
+		this.currentPosition = 0;
 		this.threshold = 100;
 		this.init();
 	}
- 
+
 	init() {
+		this.elements.wrapper.style.setProperty('--slideDuration', this.settings.slideDuration + 'ms');
+		this.elements.wrapper.style.setProperty('--slideEasing', 'var(--' + this.settings.slideEasing + ')');
+		this.elements.wrapper.classList.add(
+			this.settings.slideDirection === 'horizontal' ? 'es-horizontal' : 'es-vertical'
+		);
+
 		this.elements.sliderWrapper = this.elements.wrapper.querySelector('.es-slider');
 		this.elements.slides = this.elements.sliderWrapper.querySelectorAll('.es-slide');
 		this.elements.nextSlideButton = this.elements.wrapper.querySelector('.es-next-slide');
 		this.elements.prevSlideButton = this.elements.wrapper.querySelector('.es-prev-slide');
 		this.elements.navigationItems = this.elements.wrapper.querySelectorAll('.es-navigation-item');
-		
+
 		this.totalSlides = this.elements.slides.length;
-		this.slideWidth = this.elements.sliderWrapper.offsetWidth;
-		this.threshold = this.slideWidth / 4;
+		this.slideWidth = this.elements.wrapper.offsetWidth;
+		this.slideHeight = this.elements.wrapper.offsetHeight;
+		this.threshold = this.settings.slideDirection === 'horizontal' ? this.slideWidth / 4 : this.slideHeight / 4;
 		this.firstSlideClone = this.elements.slides[0].cloneNode(true);
 		this.lastSlideClone = this.elements.slides[this.totalSlides - 1].cloneNode(true);
- 
+
 		this.elements.sliderWrapper.appendChild(this.firstSlideClone);
 		this.elements.sliderWrapper.insertBefore(this.lastSlideClone, this.elements.slides[0]);
 
-		// Оновлюємо колекцію слайдів
-    this.elements.slides = this.elements.sliderWrapper.querySelectorAll('.es-slide');
+		// Update Sildes Collection
+		this.elements.slides = this.elements.sliderWrapper.querySelectorAll('.es-slide');
 
-    this.elements.prevSlideButton.addEventListener('click', this.moveToPrevSlide.bind(this));
-		this.elements.nextSlideButton.addEventListener('click', this.moveToNextSlide.bind(this));
-		
-		this.elements.navigationItems.forEach((item, index) => {
-			item.addEventListener('click', () => this.moveToSlideByNavigation(index + 1));
-		});
- 
-    // Mouse events
-    this.elements.sliderWrapper.addEventListener('mousedown', this.dragStart.bind(this));
-    this.elements.sliderWrapper.addEventListener('mousemove', this.dragAction.bind(this));
-    this.elements.sliderWrapper.addEventListener('mouseup', this.dragEnd.bind(this));
-    this.elements.wrapper.addEventListener('mouseleave', this.dragEnd.bind(this));
+		// Set Slider Position
+		const dimension = this.settings.slideDirection === 'horizontal' ? 'width' : 'height';
+		const position = this.settings.slideDirection === 'horizontal' ? 'left' : 'top';
+		const dimensionValue = this.settings.slideDirection === 'horizontal' ? this.slideWidth : this.slideHeight;
 
-    // Touch events
-    this.elements.wrapper.addEventListener('touchstart', this.dragStart.bind(this));
-    this.elements.wrapper.addEventListener('touchmove', this.dragAction.bind(this));
-    this.elements.wrapper.addEventListener('touchend', this.dragEnd.bind(this));
+		this.elements.sliderWrapper.style[dimension] = dimensionValue * (this.totalSlides + 2) + 'px';
+		this.elements.sliderWrapper.style[position] = -(dimensionValue * this.currentSlideIndex) + 'px';
 
-		this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
-		this.elements.sliderWrapper.style.width = this.slideWidth * (this.totalSlides + 2) + 'px';
 		this.highlightCurrentNavigationItem();
+		this._addEventsListener();
+
+		if(this.settings.autoSlide) {
+			let autoSlideIntervalId;
+
+			const stopAutoSlide = () => {
+				clearInterval(autoSlideIntervalId);
+			}
+
+			const startAutoSlide = () => {
+				autoSlideIntervalId = setInterval(() => {
+					if (this.isSlidingAllowed) {
+						this.moveToNextSlide();
+					}
+				}, this.settings.autoSlideInterval);
+			}
+			this.elements.wrapper.addEventListener('mouseenter', stopAutoSlide);
+			this.elements.wrapper.addEventListener('mouseleave', startAutoSlide);
+			startAutoSlide();
+		}
 	}
- 
+
+	_addEventsListener() {
+		// Slider Navigation
+		this.elements.prevSlideButton.addEventListener('click', this.moveToPrevSlide.bind(this));
+		this.elements.nextSlideButton.addEventListener('click', this.moveToNextSlide.bind(this));
+		this.elements.navigationItems.forEach((item, index) => {
+			item.addEventListener('click', this.moveToSlideByNavigation.bind(this, index + 1));
+		});
+
+		['mousedown', 'touchstart'].forEach(event => this.elements.sliderWrapper.addEventListener(event, this.dragStart.bind(this), { passive: true }));
+		['mousemove', 'touchmove'].forEach(event => this.elements.sliderWrapper.addEventListener(event, this.dragAction.bind(this), { passive: true }));
+		['mouseup', 'mouseleave', 'touchend'].forEach(event => this.elements.wrapper.addEventListener(event, this.dragEnd.bind(this), { passive: true }));
+
+		// Animation Events
+		this.elements.sliderWrapper.addEventListener('transitionend', this.adjustCurrentSlideIndex.bind(this));
+		// Window Resize
+		window.addEventListener('resize', () => {
+			this.slideWidth = this.elements.wrapper.offsetWidth;
+			this.slideHeight = this.elements.wrapper.offsetHeight;
+			this.threshold = this.settings.slideDirection === 'horizontal' ? this.slideWidth / 4 : this.slideHeight / 4;
+			// Set Slider Position
+			const dimension = this.settings.slideDirection === 'horizontal' ? 'width' : 'height';
+			const position = this.settings.slideDirection === 'horizontal' ? 'left' : 'top';
+			const dimensionValue = this.settings.slideDirection === 'horizontal' ? this.slideWidth : this.slideHeight;
+
+			this.elements.sliderWrapper.style[dimension] = dimensionValue * (this.totalSlides + 2) + 'px';
+			this.elements.sliderWrapper.style[position] = -(dimensionValue * this.currentSlideIndex) + 'px';
+		});
+	}
+
 	moveToNextSlide() {
 		if (!this.isSlidingAllowed) return;
 		this.isSlidingAllowed = false;
 		this.currentSlideIndex++;
 		this.updateSlidePosition();
 	}
- 
+
 	moveToPrevSlide() {
 		if (!this.isSlidingAllowed) return;
 		this.isSlidingAllowed = false;
 		this.currentSlideIndex--;
 		this.updateSlidePosition();
 	}
- 
+
 	moveToSlideByNavigation(index) {
 		if (!this.isSlidingAllowed) return;
 		this.isSlidingAllowed = false;
 		this.currentSlideIndex = index;
 		this.updateSlidePosition();
 	}
-	
+
 	dragStart(event) {
-    event.preventDefault();
-    if (!this.isSlidingAllowed) return;
+		if (!this.isSlidingAllowed) return;
 		this.isDragging = true;
 
-    this.initialPositionX = event.type === 'touchstart' ? event.touches[0].clientX : event.clientX;
-    this.currentPositionX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
+		const eventCoords = event.type === 'touchmove' ? event.touches[0] : event;
+		this.initialPosition = this.settings.slideDirection === 'horizontal' ? eventCoords.clientX : eventCoords.clientY;
+		this.currentPosition = this.initialPosition;
 	}
-	
+
 	dragAction(event) {
-    event.preventDefault();
-	  if (!this.isSlidingAllowed) return;
-	  if (!this.isDragging) return;
+		if (!this.isSlidingAllowed) return;
+		if (!this.isDragging) return;
 
-    this.currentPositionX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
+		const eventCoords = event.type === 'touchmove' ? event.touches[0] : event;
+		this.currentPosition = this.settings.slideDirection === 'horizontal' ? eventCoords.clientX : eventCoords.clientY;
 
-    const dragDifference = this.currentPositionX - this.initialPositionX;
-    const currentSliderPosition = this.slideWidth * this.currentSlideIndex;
+		const dragDifference = this.currentPosition - this.initialPosition;
+		const currentSliderPosition = this.settings.slideDirection === 'horizontal' ? this.slideWidth * this.currentSlideIndex : this.slideHeight * this.currentSlideIndex;
 
-    this.elements.sliderWrapper.style.left = -currentSliderPosition + dragDifference + 'px';
+		this.elements.sliderWrapper.style[this.settings.slideDirection === 'horizontal' ? 'left' : 'top'] = -currentSliderPosition + dragDifference + 'px';
 	}
 
-  dragEnd(event) {
-    event.preventDefault();
-	  if (!this.isDragging) return;
+	dragEnd(event) {
+		if (!this.isDragging) return;
 		this.isDragging = false;
 
-		if (this.currentPositionX - this.initialPositionX < -this.threshold) {
-      this.moveToNextSlide();
-    } else if (this.currentPositionX - this.initialPositionX > this.threshold) {
-      this.moveToPrevSlide();
-    } else {
-      this.animation(
-        this.elements.sliderWrapper,
-        'left',
-        this.elements.sliderWrapper.offsetLeft,
-        -this.slideWidth * this.currentSlideIndex,
-        'px',
-        this.settings.slideDuration / 100 * Math.abs(this.currentPositionX - this.initialPositionX) / this.threshold * 100,
-        'easeOutExpo'
-      );
-    }
-  }
+		if (this.currentPosition - this.initialPosition < -this.threshold) {
+			this.moveToNextSlide();
+		} else if (this.currentPosition - this.initialPosition > this.threshold) {
+			this.moveToPrevSlide();
+		} else {
+			this.elements.wrapper.classList.add('sliding');
+			
+			// Set Slider Position
+			const position = this.settings.slideDirection === 'horizontal' ? 'left' : 'top';
+			const dimensionValue = this.settings.slideDirection === 'horizontal' ? this.slideWidth : this.slideHeight;
+			this.elements.sliderWrapper.style[position] = -(dimensionValue * this.currentSlideIndex) + 'px';
+		}
+	}
 
 	updateSlidePosition() {
 		this.highlightCurrentNavigationItem();
 		this.elements.wrapper.classList.add('sliding');
-
-		this.animation(this.elements.sliderWrapper, 'left', this.elements.sliderWrapper.offsetLeft, -this.slideWidth * this.currentSlideIndex, 'px', this.settings.slideDuration, 'easeOutExpo', () => {
-			this.isSlidingAllowed = true;
-			this.elements.wrapper.classList.remove('sliding');
-
-			if(this.currentSlideIndex > this.totalSlides) {
-				this.currentSlideIndex = 1;
-				this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
-			} else if(this.currentSlideIndex < 1) {
-				this.currentSlideIndex = this.totalSlides;
-				this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
-			}
-		});
+		if(this.settings.slideDirection === 'horizontal'){
+			this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
+		} else {
+			this.elements.sliderWrapper.style.top = -(this.slideHeight * this.currentSlideIndex) + 'px';
+		}
 	}
- 
+
 	highlightCurrentNavigationItem() {
 		const index = (this.currentSlideIndex - 1 + this.totalSlides) % this.totalSlides;
 		this.elements.navigationItems.forEach(item => item.classList.remove('current'));
 		this.elements.navigationItems[index].classList.add('current');
 	}
- 
-	animation(element, property, startValue, endValue, unit, duration, easingName, callback) {
-		let startTime = null;
-		function update(currentTime) {
-			if(startTime === null) {
-				startTime = currentTime;
-			}
-			const elapsedTime = currentTime - startTime;
-			const progress = Math.min(elapsedTime / duration, 1);
-			const currentValue = startValue + (endValue - startValue) * easingFunctions[easingName](progress);
-			element.style[property] = currentValue + unit;
 
-			if(progress < 1) {
-				window.requestAnimationFrame(update);
-			} else {
-				if(typeof callback === "function") {
-					callback();
-				}
-			}
+	adjustCurrentSlideIndex() {
+		this.elements.wrapper.classList.remove('sliding');
+		if (this.currentSlideIndex > this.totalSlides) {
+			this.currentSlideIndex = 1;
+		} else if (this.currentSlideIndex < 1) {
+			this.currentSlideIndex = this.totalSlides;
 		}
-
-		requestAnimationFrame(update);
+		if(this.settings.slideDirection === 'horizontal') {
+			this.elements.sliderWrapper.style.left = -(this.slideWidth * this.currentSlideIndex) + 'px';
+		} else {
+			this.elements.sliderWrapper.style.top = -(this.slideHeight * this.currentSlideIndex) + 'px';
+		}
+		this.isSlidingAllowed = true;
 	}
 
 	// Method to deeply merge default options with user-provided options
-  deepMerge(target, source) {
-    if (typeof target !== 'object' || target === null) {
-      target = {};
-    }
-    
-    if (typeof source !== 'object' || source === null) {
-      return target;
-    }
+	deepMerge(target, source) {
+		if (typeof target !== 'object' || target === null) {
+			target = {};
+		}
 
-    for (const key in source) {
-      if (source[key] instanceof Object && target[key]) {
-        target[key] = this.deepMerge(target[key], source[key]);
-      } else {
-        target[key] = source[key];
-      }
-    }
+		if (typeof source !== 'object' || source === null) {
+			return target;
+		}
 
-    return target;
-  }
+		for (const key in source) {
+			if (source[key] instanceof Object && target[key]) {
+				target[key] = this.deepMerge(target[key], source[key]);
+			} else {
+				target[key] = source[key];
+			}
+		}
+
+		return target;
+	}
  }
- 
+
  const slider = new ESlider({
 	target: 'test'
  });
